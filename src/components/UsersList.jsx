1a3e4c98e449ecb3726e2f97bb93c6ff0befe9dd
JavaOf <javaof9@gmail.com>
2024-12-26 12:41:46 +0600

    users http

---
+++ src/components/UsersList.jsx
@@ -0,0 +1,45 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { fetchUsers } from '../redux/reducer'; 
+
+const UsersList = () => {
+  const dispatch = useDispatch();
+  const { users, loading, error } = useSelector((state) => state.users);
+
+  useEffect(() => {
+    dispatch(fetchUsers()); 
+  }, [dispatch]);
+
+  if (loading) {
+    return <div>
+      <div id="bowlG">
+    <div id="bowl_ringG">
+      <div class="ball_holderG">
+        <div class="ballG">
+        </div>
+      </div>
+    </div>
+  </div>
+  </div>;
+  }
+
+  if (error) {
+    return <div>Ошибка: {error}</div>;
+  }
+
+  return (
+    <div>
+      <h1>Пользователи</h1>
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            <Link to={`/user/${user.id}`}>{user.name}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default UsersList;
